93872f5bb2ee2eb3f6f925715622a1bf5cbaa8f4
Sunny <137398582+XDDAMNSON@users.noreply.github.com>
2023-07-07 12:56:35 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function scrollToTop() {
+    window.scrollTo({ top: 1, behavior: 'smooth' });
+  }
+  
+  function showAbout() {
+    var aboutContent = document.getElementById("aboutContent");
+    aboutContent.classList.toggle("hidden");
+  }
+  
+  function showContact() {
+    var contactContent = document.getElementById("contactContent");
+    contactContent.classList.toggle("hidden");
+  }
+  
